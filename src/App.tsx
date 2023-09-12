4c14b9574b5b057258ba800dc83e046158a3025e
sky(skysurfr) <skysurfr@me.com>
2023-09-12 19:52:20 +0900

    Chore: react-beautiful-dnd install setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 const App = () => {
-  return <div>App</div>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>One</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 };
 
 export default App;
